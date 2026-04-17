11b2230a8cc6d52c5f4136203216dbadb8850fdb
Luca Faliva <luca.faliva@edu.unife.it>
2026-04-17 14:24:41 +0200

    Setup project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {SampleEntitiesComponent} from './component/sample-entities/sample-entities.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
