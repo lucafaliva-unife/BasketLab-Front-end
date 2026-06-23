8191a21ee6235c0bf68dda446d95c4b8ffbbc1da
Luca Faliva <luca.faliva@edu.unife.it>
2026-06-23 17:30:30 +0200

    Refactor predict component by using promises

--- src/app/conversazioni/predict/predict.component.ts
+++ src/app/conversazioni/predict/predict.component.ts
@@ -5,6 +5,7 @@ import { TeamService } from '../../servizi/team.service';
 import { Team } from '../../modelli/team.model';
 import { Train } from '../../modelli/train.model';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     standalone: true,
@@ -16,14 +17,11 @@ import { Router } from '@angular/router';
 export class PredictComponent implements OnInit {
     selectedTeamId_1: string | null= null;
     selectedTeamId_2: string | null= null;
-    noTeams: boolean= false;
     teams: Team[]= [];
     guessedTeam: string | null= null;
     guessedTeamOk: boolean= false;
     teamAnalytics_1: Partial<Omit<Train, "id_player" | "idx_train">>= {};
     teamAnalytics_2: Partial<Omit<Train, "id_player" | "idx_train">>= {};
-    teamAnalytics_1_isVoid: boolean= false;
-    teamAnalytics_2_isVoid: boolean= false;
     percentualeTiriWeigth: number= 0.5;
     tempoCorsaWeigth: number= 0.5;
     loserTeamName: string | null= null;
@@ -40,10 +38,8 @@ export class PredictComponent implements OnInit {
     resetTeams(): void {
         this.teamService.getTeams().subscribe(teams => {
             this.teams= teams;
-            if(this.teams.length === 0) {
-                this.noTeams= true;
-            } else {
-                this.noTeams= false;
+            // Se esiste almeno un team allora rimuovo quello degli svincolati dalla lista
+            if(this.teams.length !== 0) {
                 this.teams= this.teams.filter(team => team.nome !== "Svincolati");
             }
         });
@@ -73,27 +69,65 @@ export class PredictComponent implements OnInit {
     }
 
     /*
+    Questa funzione prende in input l'ID di un team, scarica i dati del team e restituisce una promise che quando
+    diventa fullfilled ritorna un errore oppure il nome del team.
+    */
+    private async getTeamNameById(teamId: string): Promise<string> {
+        const promise: Promise<string>= new Promise<string>((resolve, reject) => {
+            this.teamService.getTeamById(teamId).subscribe({
+                next: (team) => {
+                    resolve(team.nome);
+                },
+                error: (error) => {
+                    reject(error);
+                }
+            });
+        });
+        return promise;
+    }
+
+    /*
+    Questa funzione prende in input l'ID di un team, scarica l'analytics del team e restituisce una promise che quando
+    diventa fullfilled ritorna un errore oppure l'analytics del team.
+    */
+    private async getAnalyticsByTeamId(teamId: string): Promise<Omit<Train, "id_player" | "idx_train">> {
+        const promise: Promise<Omit<Train, "id_player" | "idx_train">>= new Promise<Omit<Train, "id_player" | "idx_train">>((resolve, reject) => {
+            this.teamService.getAnalyticsByTeamId(teamId).subscribe({
+                next: (analytics) => {
+                    resolve(analytics);
+                },
+                error: (error) => {
+                    reject(error);
+                }
+            });
+        });
+        return promise;
+    }
+
+    /*
     Questa funzione calcola lo score dei due team usando i loro analytics e decide quale è il favorito.
     Infine scarica i dati dei due team per mostrare all'utente i loro nomi dati i loro ID.
     */
-    compareTeams(): void {
+    async compareTeams(): Promise<void> {
         this.tempoCorsaWeigth= 1 - this.percentualeTiriWeigth; // Calcolo il peso del tempo corsa
         const percentualeTiri_1= this.teamAnalytics_1.percentuale_tiri ?? 0; // Uso come default 0
         const tempoCorsa_1= this.teamAnalytics_1.tempo_corsa ?? 1; // Uso come default 1
         const percentualeTiri_2 = this.teamAnalytics_2.percentuale_tiri ?? 0; // Uso come default 0
         const tempoCorsa_2 = this.teamAnalytics_2.tempo_corsa ?? 1; // Uso come default 1
+        // Calcolo lo score del team 1
         const teamScore_1= (
             percentualeTiri_1 * this.percentualeTiriWeigth
             +
             (100 / tempoCorsa_1) * this.tempoCorsaWeigth
         ) / (this.percentualeTiriWeigth + this.tempoCorsaWeigth);
+        // Calcolo lo score del team 2
         const teamScore_2= (
             percentualeTiri_2 * this.percentualeTiriWeigth
             +
             (100 / tempoCorsa_2) * this.tempoCorsaWeigth
         ) / (this.percentualeTiriWeigth + this.tempoCorsaWeigth);
+        // Valuto quale sia il team con lo score più alto. Favorisco chi gioca in casa usando '>='
         if(teamScore_1 >= teamScore_2) {
-            //Favorisco chi gioca in casa usando '>='
             this.winnerTeamId= this.selectedTeamId_1;
             this.loserTeamId= this.selectedTeamId_2;
             this.teamScoreWinner= teamScore_1;
@@ -104,102 +138,87 @@ export class PredictComponent implements OnInit {
             this.teamScoreWinner= teamScore_2;
             this.teamScoreLoser= teamScore_1;
         }
-        // Recupero i nomi dei due team
-        this.teamService.getTeamById(this.winnerTeamId!).subscribe({
-            next: (team) => {
-                // Recupero il nome del team vincitore
-                this.guessedTeam= team.nome;
-                this.guessedTeamOk= true;
-                // Recupero il nome del team perdente
-                this.teamService.getTeamById(this.loserTeamId!).subscribe({
-                    next: (team) => {
-                        this.loserTeamName= team.nome;
-                    },
-                    error: (err) => {
-                        if(err.status === 404) {
-                            alert("Errore: team ipotizzato come perdente non esistente");
-                        } else {
-                            alert("Errore: " + err.error);
-                        }
-                        this.router.navigate(["/teams"]);
-                    }
-                });
-            },
-            error: (err) => {
-                if(err.status === 404) {
-                    alert("Errore: team ipotizzato come vincitore non esistente");
-                } else {
-                    alert("Errore: " + err.error);
-                }
-                this.router.navigate(["/teams"]);
+        // Recupero il nome del team vincitore
+        try {
+            this.guessedTeam= await this.getTeamNameById(this.winnerTeamId!);
+        } catch(err) {
+            // Inizialmente err è di tipo unknown, quindi lo devo trattare come un HttpErrorResponse
+            if((err as HttpErrorResponse).status === 404) {
+                alert("Errore: team ipotizzato come vincitore non esistente");
+            } else {
+                alert("Errore: " + (err as HttpErrorResponse).error);
             }
-        });
+            this.router.navigate(["/teams"]);
+            return;
+        }
+        // Recupero il nome del team perdente
+        try {
+            this.loserTeamName= await this.getTeamNameById(this.loserTeamId!);
+        } catch(err) {
+            // Inizialmente err è di tipo unknown, quindi lo devo trattare come un HttpErrorResponse
+            if((err as HttpErrorResponse).status === 404) {
+                alert("Errore: team ipotizzato come perdente non esistente");
+            } else {
+                alert("Errore: " + (err as HttpErrorResponse).error);
+            }
+            this.router.navigate(["/teams"]);
+            return;
+        }
+        // Visualizzo il nome del team vincitore ed i punteggi di entrambi i team
+        this.guessedTeamOk= true;
     }
 
     /*
     Questa funzione scarica gli analytics dei due team, si assicura che entrambi abbiano un analytics valido (quindi con
     degli allenamenti applicati ai suoi player) ed infine li confronta per decidere il vincitore.
     */
-    predict(): void {
+    async predict(): Promise<void> {
         if(this.selectedTeamId_1 && this.selectedTeamId_2) {
-            if(this.selectedTeamId_1 !== this.selectedTeamId_2) {
-                // Scarico l'analytics del team 1
-                this.teamService.getAnalyticsByTeamId(this.selectedTeamId_1).subscribe({
-                    next: (analytics) => {
-                        this.teamAnalytics_1= analytics;
-                        if(Object.keys(this.teamAnalytics_1).length === 0) {
-                            this.teamAnalytics_1_isVoid= true;
-                            // Mi fermo e non scarico nemmeno l'analytics del team 2
-                            this.guessedTeamOk= false;
-                            this.guessedTeam= null;
-                            this.loserTeamName= null;
-                        } else {
-                            this.teamAnalytics_1_isVoid= false;
-                            // Scarico l'analytics del team 2
-                            this.teamService.getAnalyticsByTeamId(this.selectedTeamId_2!).subscribe({
-                                next: (analytics) => {
-                                    this.teamAnalytics_2= analytics;
-                                    if(Object.keys(this.teamAnalytics_2).length === 0) {
-                                        this.teamAnalytics_2_isVoid= true;
-                                        this.guessedTeamOk= false;
-                                        this.guessedTeam= null;
-                                        this.loserTeamName= null;
-                                    } else {
-                                        this.teamAnalytics_2_isVoid= false;
-                                        // Se entrambi i team hanno un analytics allora li confronto
-                                        this.compareTeams();
-                                    }
-                                },
-                                error: (err) => {
-                                    if(err.status === 404) {
-                                        alert("Errore: team non esistente");
-                                    } else {
-                                        alert("Errore " + err.status);
-                                    }
-                                    this.resetTeams();
-                                    this.reset();
-                                }
-                            });
-                        }
-                    },
-                    error: (err) => {
-                        if(err.status === 404) {
-                            alert("Errore: team non esistente");
-                        } else {
-                            alert("Errore " + err.status);
-                        }
-                        this.resetTeams();
-                        this.reset();
-                    }
-                });
-            } else {
+            // Controllo che i due team selezionati siano diversi
+            if(this.selectedTeamId_1 === this.selectedTeamId_2) {
                 alert("Selezionare due team diversi");
-                this.guessedTeam= null;
-                this.guessedTeamOk= false;
+                return;
             }
+            // Scarico l'analytics del team 1
+            try {
+                this.teamAnalytics_1= await this.getAnalyticsByTeamId(this.selectedTeamId_1);
+                // Se il team non ha allenamenti allora mi fermo e do un errore
+                if(Object.keys(this.teamAnalytics_1).length === 0) {
+                    alert("Il team 1 non ha allenamenti: scegliere un altro team");
+                    return;
+                }
+            } catch(err) {
+                // Inizialmente err è di tipo unknown, quindi lo devo trattare come un HttpErrorResponse
+                if((err as HttpErrorResponse).status === 404) {
+                    alert("Errore: team 1 non esistente");
+                } else {
+                    alert("Errore " + (err as HttpErrorResponse).status);
+                }
+                this.reset();
+                return;
+            }
+            // Scarico l'analytics del team 2
+            try {
+                this.teamAnalytics_2= await this.getAnalyticsByTeamId(this.selectedTeamId_2);
+                // Se il team non ha allenamenti allora mi fermo e do un errore
+                if(Object.keys(this.teamAnalytics_2).length === 0) {
+                    alert("Il team 2 non ha allenamenti: scegliere un altro team");
+                    return;
+                }
+            } catch(err) {
+                // Inizialmente err è di tipo unknown, quindi lo devo trattare come un HttpErrorResponse
+                if((err as HttpErrorResponse).status === 404) {
+                    alert("Errore: team 2 non esistente");
+                } else {
+                    alert("Errore " + (err as HttpErrorResponse).status);
+                }
+                this.reset();
+                return;
+            }
+            // Se sono arrivato fino a qui allora entrambi i team hanno un analytics e quindi li confronto
+            this.compareTeams();
         } else {
-            alert("Errore: selezionare entrambi i team");
-            this.guessedTeam= null;
+            alert("Errore: selezionare entrambi i teams");
             this.guessedTeamOk= false;
         }
     }
